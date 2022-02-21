8b8cfebd3ab87004c3190ee152fa2554a22ff49b
tkpea <tkhtouch@me.com>
2022-02-21 17:28:24 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/wordle-tool/',
   build: {
     outDir: 'docs',
   },
